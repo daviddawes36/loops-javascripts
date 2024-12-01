b2ed8d056b9652693512b9e2506f5181d4e6becc
daviddawes36 <daviddawes33@gmail.com>
2024-12-01 21:52:48 +0000

    printing out 12 7 times

---
+++ loops-Javascript.js
@@ -0,0 +1,24 @@
+// const evens = [2, 4, 6, 8, 10, 12]
+console.log('print numbers 0 to 6' )
+for (let i = 0; i<7; i++ ){
+  console.log(i)
+}
+
+console.log('print numbers 0 to 8' )
+for (let i = 0; i<9; i++ ){
+  console.log(i)
+}
+
+// print out the number 12 (7 times)
+
+
+console.log('print out the number 12 (7 times)')
+for (let i = 0; i<7; i++){
+    // console.log(i)
+    console.log(12)
+}
+// print the number 100 (5 times)
+// print out the number (0,1, 2, 3..... 10)
+// print out the number (1,2,3,4,....10)
+// print out the even number (0,2,4,6,8,10)
+// print out the odd numbers (1,3,5,7,9)
