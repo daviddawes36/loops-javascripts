81b7055734afd79043af11358efb78d2e8c31d6d
daviddawes36 <daviddawes33@gmail.com>
2025-01-26 21:17:53 +0000

    for loop exercises

--- loops-Javascript.js
+++ loops-Javascript.js
@@ -1,29 +1,104 @@
-// const evens = [2, 4, 6, 8, 10, 12]
-console.log('print numbers 0 to 6' )
-for (let i = 0; i<7; i++ ){
-  console.log(i)
-}
+// // const evens = [2, 4, 6, 8, 10, 12]
+// console.log('print numbers 0 to 6' )
+// for (let i = 0; i<7; i++ ){
+//   console.log(i)
+// }
 
-console.log('print numbers 0 to 8' )
-for (let i = 0; i<9; i++ ){
-  console.log(i)
-}
+// console.log('print numbers 0 to 8' )
+// for (let i = 0; i<9; i++ ){
+//   console.log(i)
+// }
 
-// print out the number 12 (7 times)
+// // print out the number 12 (7 times)
 
 
-console.log('print out the number 12 (7 times)')
-for (let i = 0; i<7; i++){
-    // console.log(i)
-    console.log(12)
-}
-// print the number 100 (5 times)
+// console.log('print out the number 12 (7 times)')
+// for (let i = 0; i<7; i++){
+//   // console.log('this is loop number')
+//     // console.log(i)
+//     console.log(12)
+// }
+// // print the number 100 (5 times)
 
-console.log('print the number 100 (5 times)')
-for (let i = 0; i<5; i++){
-    console.log(100)
-}
-// print out the number (0,1, 2, 3..... 10)
-// print out the number (1,2,3,4,....10)
-// print out the even number (0,2,4,6,8,10)
+// console.log('print the number 100 (5 times)')
+// for (let i = 0; i<5; i++){
+//     console.log(100)
+// }
+// // print out the number (0,1, 2, 3..... 10)
+// console.log('print out the number (0,1, 2, 3..... 10)')
+// for (let i = 0; i<11; i++){
+//   console.log(i)
+// }
+// // print out the number (1,2,3,4,....10)
+// console.log('print out the number (1,2,3,4,....10)')
+// for (let i = 1; i<11; i++){
+//   console.log(i)
+// }
+// // print out the even number (0,2,4,6,8,10)
+// console.log('print out the even number (0,2,4,6,8,10)')
+// for (let i = 0; i<6; i++){
+//   // console.log(i)
+//   console.log(i*2)
+// }
+// console.log('print out the even number (0,2,4,6,8,10)')
+// for (let i = 0; i<11; i=i+2){
+//   console.log(i)
+// }
 // print out the odd numbers (1,3,5,7,9)
+console.log('print out the odd numbers (1,3,5,7,9)')
+for (let i = 1; i<10; i=i+2){
+  console.log(i)
+  
+}
+
+// Print out the numbers 0, 5, 10 ,15 20, 25, all the way up to 100
+console.log('Print out the numbers 0, 5, 10 ,15 20, 25, all the way up to 100')
+for (let i = 0; i<101; i=i+5){
+  console.log(i)
+}
+
+// print out the numbers 3, 13, 23,33,43,53,63,73,83,93
+console.log('print out the numbers 3, 13, 23,33,43,53,63,73,83,93')
+for (let i = 3; i<94; i=i+10){
+  console.log(i)
+}
+
+// Print out the even numbers starting from 6 up to 14 (6,8,10,12,14)
+console.log('Print out the even numbers starting from 6 up to 14 (6,8,10,12,14)') 
+for (let i = 6; i<15; i=i+2){
+  console.log(i)
+}
+
+// Print out (10,20,30,40,50,60,70,80,90,100)
+console.log('Print out (10,20,30,40,50,60,70,80,90,100)')
+for (let i = 10; i<101; i=i+10){
+  console.log(i)
+}
+
+// Print out the text ('you are user number 1', 'you are user number 2', ... '
+// you are user number 100')
+console.log('Print out the text (you are user number 1 ect')
+for (let i = 1; i<= 100; i++){
+  console.log('you are user number ' +i);
+  console.log(`your number is ${i} `)
+}
+
+// Print out (1,2,3,4,50)
+console.log('Print out 1,2,3,4,50')
+for (let i = 1; i<51; i++){
+  console.log(i)
+}
+
+// print out Hello world
+console.log('print out text (Hello World)')
+for (let i = 1; i<= 20; i++){
+  console.log(`Hello World ${i}`)
+  // console.log('Hello World')
+}
+
+
+// print out 50 to 100
+console.log('print out 50 to 100')
+for (let i = 50; i<101; i++){
+  console.log(i)
+}
