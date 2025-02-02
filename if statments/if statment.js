72e30138613a367424ac7170090cf1f0d568fbc3
daviddawes36 <daviddawes33@gmail.com>
2025-02-02 22:39:35 +0000

    positive negative and odd even number check exercises

---
+++ if statments/if statment.js
@@ -0,0 +1,37 @@
+// // exercise from https://medium.com/@andrei_diaconu/it-beginner-series-javascript-if-else-exercises-cfc5d65b6f94
+// const num = 10 
+
+// if (num>0){
+//     console.log('this is a positive number')
+// } else {
+//     console.log('this is not a positive number')
+// }
+
+// const trafficLight = 'red'
+
+// if (trafficLight=='green'){
+//     console.log('go')
+// }else if (trafficLight=='red'){
+// console.log('stop')
+// }
+
+// Exercise list
+console.log('Write a program that determines whether a given number is positive or negative.')
+const positiveOrNegative = +13 
+console.log(positiveOrNegative)
+
+if (positiveOrNegative>0){
+console.log('positive')
+} else {
+console.log('negative')
+}
+
+console.log('Write a program that checks if a number is even or odd.')
+const oddOrEven = 3
+
+console.log(oddOrEven)
+if (oddOrEven%2==1){
+    console.log('odd')
+}else{
+console.log('even')
+}
